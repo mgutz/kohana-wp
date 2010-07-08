db9cd25aa7d48378e8d1ebc79f6eec41b6f44512
mgutz <mario@mgutz.com>
2010-07-07 21:23:11 -0700

    removed dependency on KOstache, simplified Mustache view classes even more

--- modules/kwp/classes/controller/mustache.php
+++ modules/kwp/classes/controller/mustache.php
@@ -11,43 +11,64 @@
  * Adds some nice features like creating an auto Mustache view without having to define a class.
  */
 class Controller_Mustache extends Controller {
-	// must use triple braces {{{ }}} on urls so they are not escaped
+	/**
+	 * URL paths compatible with WordPress. Must be used for applications to work correctly in WordPress.
+	 */
 	public $app_url = KWP_APP_URL;
 	public $controller_url = KWP_CONTROLLER_URL;
-	
+	public $page_url = KWP_PAGE_URL;
+
 	/**
-	 * A normal view has no logic behind it and does not require a code-behind class. 
+	 * Renders a Mustache view using a template only.
 	 *
-	 * @parm $path The relative path to the template from 
+	 * @parm $template_path The path of the template relative to classes/view.
+	 * @param $locals Local variables.
 	 * @return void
 	 */
-	function view($template_path, $locals = NULL) {
-		$template = Kohana::find_file('classes/view', $template_path, 'mustache');
-
-		if ($template)
-			$content = file_get_contents($template);
-		else
-			throw new Kohana_Exception('Template file not found: view/' . $template_path);
-
-		$mustache = new Mustache;
-
-		foreach ($this as $key => $value) {
-			if ($key == 'request') {
-				continue;
-			}
-			$instance_vars[$key] = $value;
-		}
-
-		return $mustache->render($content, empty($locals) ? $instance_vars : Arr::merge($locals, $instance_vars));
-	}
-
-
 	function render($template_path, $locals = NULL) {
 		$this->request->response = $this->view($template_path, $locals);
 	}
 
 
+	/**
+	 * Renders a Mustache view with both a class and template.
+	 *
+	 * @param  $class_path Path of PHP class relative to view.
+	 * @param  $locals Local variables.
+	 * @return void
+	 */
+	function render_class($class_path, $locals = NULL) {
+		$this->request->response = $this->view_class($class_path, $locals);
+	}
+
+	/**
+	 * Creates a Mustache view using a template only.
+	 *
+	 * @parm $template_path The path of the template relative to classes/view.
+	 * @param $locals Local variables.
+	 * @return void
+	 */
+	function view($template_path, $locals = NULL) {
+		$class = new stdClass();
+		return $this->view_common($class, $template_path, $locals);
+	}
+
+	/**
+	 * Creates a Mustache view using both a class and template.
+	 *
+	 * @param  $template_path The path of the template and class relative to classes/view.
+	 * @param  $locals Local variables.
+	 * @return string
+	 */
 	function view_class($template_path, $locals=NULL) {
+		$name = str_replace('/', '_', 'view/' . $template_path);
+		$class = new $name();
+
+		return $this->view_common($class, $template_path, $locals);
+	}
+
+
+	private function view_common($class, $template_path, $arr = NULL) {
 		$template = Kohana::find_file('classes/view', $template_path, 'mustache');
 
 		if ($template)
@@ -55,47 +76,23 @@ class Controller_Mustache extends Controller {
 		else
 			throw new Kohana_Exception('Template file not found: view/' . $template_path);
 
-
 		foreach ($this as $key => $value) {
-			if ($key == 'request') {
-				continue;
+			if ($key != 'request') {
+				$instance_vars[$key] = $value;
 			}
-			$instance_vars[$key] = $value;
 		}
 
-		$name = Inflector::underscore('view_' . $template_path);
-		$name = str_replace('/', '_', $name);
-		$vars = empty($locals) ? $instance_vars : Arr::merge($locals, $instance_vars);
-		$class = new $name($vars);
+		$vars = empty($arr) ? $instance_vars : Arr::merge($arr, $instance_vars);
 
+		// Add $vars to instantiated class, but do not overwrite existing properties
 		foreach ($vars as $key => $value) {
-			$class->$key = $value;
+			if (!property_exists($class, $key)) {
+				$class->$key = $value;
+			}
 		}
 
 		$mustache = new Mustache;
 		return $mustache->render($content, $class, $vars);
-	}
-
-	/**
-	 * Renders a view using a code-behind class.
-	 *
-	 * @param  $class_path Path of PHP class relative to view.
-	 * @param  $locals
-	 * @return void
-	 */
-	function render_class($class_path, $vars = NULL) {
-		$this->request->response = $this->view_class($class_path, $vars);
-	}
-
-	/**
-	 * TODO: need a way to exclude instance variables.
-	 * @param  $locals
-	 * @return void
-	 */
-	function assign_instance_variables($locals) {
-		foreach ($this as $key => $value) {
-			$locals[$key] = $value;
-		}
 	}
 
 }
