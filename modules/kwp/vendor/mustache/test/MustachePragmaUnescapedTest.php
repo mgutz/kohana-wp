32972c4dbdff1fdba03150f1b84413a0155b0f89
mgutz <mario@mgutz.com>
2010-07-07 21:23:52 -0700

    oops, mustache classes

---
+++ modules/kwp/vendor/mustache/test/MustachePragmaUnescapedTest.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once '../Mustache.php';
+
+class MustachePragmaUnescapedTest extends PHPUnit_Framework_TestCase {
+
+	public function testPragmaUnescaped() {
+		$m = new Mustache(null, array('title' => 'Bear > Shark'));
+		
+		$this->assertEquals('Bear > Shark', $m->render('{{%UNESCAPED}}{{title}}'));
+		$this->assertEquals('Bear &gt; Shark', $m->render('{{title}}'));
+		$this->assertEquals('Bear &gt; Shark', $m->render('{{%UNESCAPED}}{{{title}}}'));
+		$this->assertEquals('Bear > Shark', $m->render('{{{title}}}'));
+	}
+
+}
