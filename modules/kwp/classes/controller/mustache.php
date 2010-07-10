f86dd879fbae8fb788210807e3cb669616e8f733
mgutz <mario@mgutz.com>
2010-07-09 17:34:43 -0700

    refactored mustache handling

--- modules/kwp/classes/controller/mustache.php
+++ modules/kwp/classes/controller/mustache.php
@@ -19,94 +19,28 @@ class Controller_Mustache extends Controller {
 	public $page_url = KWP_PAGE_URL;
 
 	/**
-	 * Renders a Mustache view.
+	 * Renders a Mustache template and assigns it to the response stream.
 	 *
 	 * If a PHP class file resides in the same directory
 	 * as the template, the class will be instantiated. If not, the template
 	 * will be used directly.
 	 *
-	 * @parm $template_path The path of the template relative to classes/view.
-	 * @param $locals Local variables.
-	 * @param $ignore_class Ignore loading the class.
+	 * @param string $template_path The path of the template relative to classes/view.
+	 * @param array $locals Local variables.
 	 * @return void
 	 */
-	function render($template_path, $locals = NULL, $ignore_class = false) {
-		if (!$ignore_class)
-			$class_file = Kohana::find_file('classes/view', $template_path);
-		
-		if ($class_file)
-			$view = $this->view_class($template_path, $locals);
-		else
-			$view = $this->view($template_path, $locals);
-		$this->request->response = $view;
-	}
-
-
-	/**
-	 * Renders a Mustache view with both a class and template.
-	 *
-	 * @param  $class_path Path of PHP class relative to view.
-	 * @param  $locals Local variables.
-	 * @return void
-	 */
-	function render_class($class_path, $locals = NULL) {
-		$this->request->response = $this->view_class($class_path, $locals);
+	function render($template_path, $locals = NULL) {
+		$this->request->response = $this->render_text($template_path, $locals);
 	}
 
 	/**
-	 * Creates a Mustache view using a template only.
+	 * Renders a Mustache template as a string.
 	 *
-	 * @parm $template_path The path of the template relative to classes/view.
-	 * @param $locals Local variables.
-	 * @return void
-	 */
-	function view($template_path, $locals = NULL) {
-		$class = new stdClass();
-		return $this->view_common($class, $template_path, $locals);
-	}
-
-	/**
-	 * Creates a Mustache view using both a class and template.
-	 *
-	 * @param  $template_path The path of the template and class relative to classes/view.
-	 * @param  $locals Local variables.
+	 * @param string $template_path The path of the template relative to classes/view.
+	 * @param array $locals Local variables.
 	 * @return string
 	 */
-	function view_class($template_path, $locals=NULL) {
-		$name = str_replace('/', '_', 'view/' . $template_path);
-		$class = new $name();
-
-		return $this->view_common($class, $template_path, $locals);
+	function render_text($template_path, $locals = NULL) {
+		return KwpMustache::render($template_path, $this, $locals);
 	}
-
-
-	private function view_common($class, $template_path, $arr = NULL) {
-		$template = Kohana::find_file('classes/view', $template_path, 'mustache');
-
-		if ($template)
-			$content = file_get_contents($template);
-		else
-			throw new Kohana_Exception('Template file not found: view/' . $template_path);
-
-		foreach ($this as $key => $value) {
-			if ($key != 'request') {
-				$instance_vars[$key] = $value;
-			}
-		}
-
-		$vars = empty($arr) ? $instance_vars : Arr::merge($arr, $instance_vars);
-
-		// Add $vars to instantiated class, but do not overwrite existing properties
-		foreach ($vars as $key => $value) {
-			if (!property_exists($class, $key)) {
-				$class->$key = $value;
-			}
-		}
-
-		$mustache = new Mustache;
-		$base = dirname(APPPATH . 'classes/view/' . $template_path);
-		$mustache->_setTemplateBase($base);
-		return $mustache->render($content, $class, $vars);
-	}
-
 }
